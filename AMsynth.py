ecb10909d75dda8ec2e79af570f0c91d964e1173
timelordible <jaysonfaupel@gmail.com>
2026-02-15 21:52:49 -0500

    Update AMsynth.py

--- AMsynth.py
+++ AMsynth.py
@@ -1,1 +1,25 @@
+# TODO: Replace the code below with your implementation of a AM synthesis
+def am_synth(carrier_type, carrier_freq, mod_depth, mod_ratio, dur, fs=44100, amp=1, modulator_type='sine'):
 
+
+  out = 
+    sig = gen_wave(carrier_type, carrier_freq, dur, fs=fs)
+    return sig
+
+
+
+    """
+    Args:
+    carrier_type (str) = carrier waveform type: 'sine', 'square', 'saw', or 'triangle'
+    carrier_freq (float) = frequency of carrier in Hz
+    mod_depth (float) = depth of the modulator
+    mod_ratio (float) = modulation ratio, where 1:mod_ratio is C:M
+    dur (float) = duration of the sinusoid (in seconds)
+    fs (float) = sampling frequency of the sinusoid in Hz
+    amp (float) = amplitude of the carrier
+    modulator_type (str) = modulator waveform type: 'sine', 'square', 'saw', or 'triangle'
+
+    Returns:
+    The function should return a numpy array
+    sig (numpy array) = amplitude modulated signal
+    """
