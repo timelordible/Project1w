ddfb482977224d9bf88273f6fb1a05f56957118b
timelordible <jaysonfaupel@gmail.com>
2026-02-15 22:47:17 -0500

    Update AMsynth.py

--- AMsynth.py
+++ AMsynth.py
@@ -1,10 +1,9 @@
 # TODO: Replace the code below with your implementation of a AM synthesis
 def am_synth(carrier_type, carrier_freq, mod_depth, mod_ratio, dur, fs=44100, amp=1, modulator_type='sine'):
 
-
-    mod_freq = carrier_freq/mod_ratio
-
+    mod_freq = carrier_freq*mod_ratio
     time_arr = np.arange(0, dur, 1/fs)
+    
     if modulator_type.casefold() == 'sine':
         mod = np.sin(np.pi*2 * mod_freq * time_arr)
     elif modulator_type.casefold() == 'square':
@@ -12,13 +11,24 @@ def am_synth(carrier_type, carrier_freq, mod_depth, mod_ratio, dur, fs=44100, am
     elif modulator_type.casefold() == 'saw':
         mod = saw(2*np.pi * mod_freq * time_arr)
     elif modulator_type.casefold() == 'triangle':
-        mod = saw(2*np.pi * mod_freq * time_arr, width=0.5)
+        mod = tri(2*np.pi * mod_freq * time_arr, width=0.5)
     else:
         print('Please enter sine, square, saw, or tri for modulator wave type')
         return None
-  
-    am = (mod_depth * mod)
-    sig = gen_wave(carrier_type, carrier_freq, dur, fs=fs, am)
+
+    if carrier_type.casefold() == 'sine':
+        carrier = np.sin(np.pi*2 * carrier_freq * time_arr)
+    elif carrier_type.casefold() == 'square':
+        carrier = square(2*np.pi * carrier_freq * time_arr)
+    elif carrier_type.casefold() == 'saw':
+        carrier = saw(2*np.pi * carrier_freq * time_arr)
+    elif carrier_type.casefold() == 'triangle':
+        carrier = tri(2*np.pi * carrier_freq * time_arr, width=0.5)
+    else:
+        print('Please enter sine, square, saw, or tri for carrier wave type')
+        return None
+    env = 1 + mod_depth * mod
+    sig = (amp * env * carrier)
     return sig
 
 
@@ -37,4 +47,5 @@ def am_synth(carrier_type, carrier_freq, mod_depth, mod_ratio, dur, fs=44100, am
     Returns:
     The function should return a numpy array
     sig (numpy array) = amplitude modulated signal
-    """
+
+     sig = gen_wave(carrier_type, carrier_freq, dur, fs=fs, am)
